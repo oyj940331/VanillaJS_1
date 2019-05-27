183a08a0ac066357c8e58414ebeb9a2476d7a984
Ok Young Jin <oyj940331@gmail.com>
2019-05-27 21:59:20 +0900

    Getting the Weather part One

---
+++ weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "4f8e54bfea212bc763f7ded4939a9bd5";
+
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("Cant access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
